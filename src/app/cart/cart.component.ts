728403f2f5ef70d96ee29db0d235a0a4c8e7d449
Mohamed-Hasan <mohamed.hassan.pet@gmail.com>
2018-03-22 23:51:38 +0200

    checkout page done

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,11 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  purchases:object[];
-  
+  purchases;
+  checked=false;
   constructor() { }
 
   ngOnInit() {
+    let prod1 = {
+      name: "iphone",
+      price: 800,
+      rating:3.5,
+      image:null,
+      quantity:1,
+      available:5,
+    }
+
+    let prod2 = {
+      name: "iphone s6",
+      price: 1000,
+      rating:4,
+      image:null,
+      quantity:1,
+      available:4,
+    }
+    
+    this.purchases=[prod1,prod2];
+  }
+
+  cancelOrder(prodName)
+  {
+    for (let i = 0; i < this.purchases.length; i++) {
+      const element = this.purchases[i];
+      if (element.name == prodName) {
+        this.purchases.splice(i,1)
+      }
+      console.log(this.purchases);
+    }
+  }
+
+  totalToPay(){
+    let sum=0;
+    for (let i = 0; i < this.purchases.length; i++) {
+      sum += this.purchases[i].price;
+    }
+    return sum;
+  }
+
+  checkout(){
+    //send purchases to api to save in db then show thanks message
+    this.checked = true;
   }
 
 }
