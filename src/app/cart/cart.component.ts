b832b037d2bfd8b880090f2b6f6d30b8ff422d9e
Mohamed-Hasan <mohamed.hassan.pet@gmail.com>
2018-03-22 22:05:34 +0200

    subcategory page done

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  purchases:object[];
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/subcategory/subcategory.component.ts
+++ src/app/subcategory/subcategory.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubcategoryComponent implements OnInit {
 
+  catName;
+  subcatName;
+  products:object[];
+
   constructor() { }
 
   ngOnInit() {
+    //send to api to get all products of a certain subcategory... should be array of objects
+    //subcat name to be sent to server
+    let prod1 = {
+      name: "iphone",
+      price: 800,
+      rating:3.5,
+      image:null,
+      available:5,
+      desc:"afefwfregergegegregergergergergergergergergergrregregregergregergergergergergergergergergre"
+    }
+
+    let prod2 = {
+      name: "iphone s6",
+      price: 1000,
+      rating:4,
+      image:null,
+      available:5,
+      desc:"afefwfregergegegregergergergergergergergergergrregregregergregergergergergergergergergergre"
+    }
+    
+    this.products=[prod1,prod2];
+    this.catName="electronics";
+    this.subcatName="mobiles"
   }
 
 }
