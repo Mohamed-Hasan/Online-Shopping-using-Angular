4bbba96b368f18a9e010d37218e95c51684dd561
Mohamed-Hasan <mohamed.hassan.pet@gmail.com>
2018-03-23 01:24:54 +0200

    add product page

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-
+  added=false;
+  product={
+  name:null,
+  price:0,
+  quantity:1,
+  description:null,
+  image:null,
+  };
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  submit(){
+    console.log(this.product);
+    this.product={
+      name:null,
+      price:0,
+      quantity:1,
+      description:null,
+      image:null,
+      };
+    //send product to api 
+    this.added = true;
+  }
+
+  _handleReaderLoaded(readerEvt) {
+    const binaryString = readerEvt.target.result;
+    this.product.image = btoa(binaryString);
+  }
+
+  onFileChange(event) {
+    const reader = new FileReader();
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsBinaryString(event.file);
+      }
+
+  onRemoved(event) {
+    this.product.image = null;
+  }
 }
